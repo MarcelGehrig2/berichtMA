4ce435fe62e9f8aae42a2bf34db5eaf2a6c928f2
MarcelGehrig <marcel.gehrig@ntb.ch>
2018-06-22 09:18:59 +0200

    WIP

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[[ $_ != $0 ]] && script_is_sourced=true || script_is_sourced=false
+if [ $script_is_sourced ]; then
+    SCRIPT_PATH=$BASH_SOURCE
+else
+    SCRIPT_PATH="$(readlink -f $0)"
+fi
+SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
+SCRIPT_NAME=$(basename $SCRIPT_PATH)
+
+
+git add . && git commit -m "WIP" && git push
+
